b1e9d6064896d031e7184181c334d58789d7e19b
avisionh <a_vision@hotmail.co.uk>
2020-09-01 08:32:44 +0100

    feat: Load data to extract data
    This is so we can make a start on this report for the team.

---
+++ notebooks/inaccessible_nonhtml.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+
+# create dictionaries and headers to specify dtype and date columns
+dict_header = {'base_path': object,
+               'content_id': object,
+               'title': object,
+               'description': object,
+               'publishing_app': object,
+               'document_type': object,
+               'details': object,
+               'text': object,
+               'organisations': object,
+               'taxons': object,
+               'step_by_steps': object,
+               'details_parts': object,
+               'first_published_at': object,
+               'public_updated_at': object,
+               'updated_at': object,
+               'finder': object,
+               'facet_values': object,
+               'facet_groups': object,
+               'has_brexit_no_deal_notice': bool,
+               'withdrawn': bool,
+               'withdrawn_at': object,
+               'withdrawn_explanation': object}
+list_header_date = ['first_published_at',
+                    'public_updated_at',
+                    'updated_at',
+                    'withdrawn_at']
+
+# load data
+df = pd.read_csv(filepath_or_buffer='data/preprocessed_content_store_200820.csv.gz',
+                 compression='gzip',
+                 encoding='utf-8',
+                 sep='\t',
+                 header=0,
+                 names=list(dict_header.keys()),
+                 dtype=dict_header,
+                 parse_dates=list_header_date)
+
+# report should contain following columns:
+#   - base_path
+#   - pubishing_app (mainstream(?), service-manual-publisher, specialist-publisher, travel-advice-publisher)
+#   - organisations
+#   - attachment title (from details)
+#   - attachment file + extension (from details)
+#   - first_published_at
+#   - public_updated_at
+#   - updated_at
+
+# select only relevant columns
