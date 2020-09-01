ec8ada9420b5fa0fa7cee2fbccd0fea392a6f098
avisionh <a_vision@hotmail.co.uk>
2020-09-01 09:23:57 +0100

    feat: Filter only pages with attachments
    This is so we can focus on the pages we care about and consequently improve the speed at which we apply functions since we are applying them on a smaller subset of data.

--- notebooks/inaccessible_nonhtml.py
+++ notebooks/inaccessible_nonhtml.py
@@ -1,5 +1,47 @@
 import pandas as pd
 
+import os
+import ast
+from bs4 import BeautifulSoup
+
+
+# function to extract links from HTML
+def get_links(text):
+    list_links = []
+    soup = BeautifulSoup(text, "html.parser")
+    for a in soup.find_all('a', href=True):
+        link = a['href']
+        list_links.append(link)
+    return list_links
+
+
+# function to extract filename from HTML
+def extract_filename(list_text):
+    """Extracts the last part of a URL path string, including the file name and extension
+
+    :param list: A list of strings to extract last part from, e.g. everything after '/'
+    :return: A list of the same length as list_text, but with the last parts kept e.g. everything after '/'
+
+    """
+    file_name = [os.path.split(text)[1] for text in list_text]
+    return file_name
+
+
+# function to extract titles
+def extract_title(text):
+    """Extracts all the attachment titles from GOV.UK pages
+
+    :param html: String of the HTML code for the GOV.UK page being passed in
+    :return: list of all the attachment titles that were extracted from GOV.UK page
+
+    """
+    text = ast.literal_eval(text)
+    text = text.get('attachments')
+
+    titles = list(map(lambda x: x['title'], text))
+
+    return titles
+
 
 # create dictionaries and headers to specify dtype and date columns
 dict_header = {'base_path': object,
@@ -49,4 +91,27 @@ df = pd.read_csv(filepath_or_buffer='data/preprocessed_content_store_200820.csv.
 #   - public_updated_at
 #   - updated_at
 
+del dict_header, list_header_date
+
 # select only relevant columns
+df_output = df[['base_path',
+                'organisations',
+                'publishing_app',
+                'document_type',
+                'first_published_at',
+                'public_updated_at',
+                'updated_at',
+                'details']].copy()
+
+# parameter for all GOV.UK file attachments to identify
+file_attachment = (".chm|.csv|.diff|.doc|.docx|.dot|.dxf|.eps|"
+                   + ".gif|.gml|.ics|.jpg|.kml|.odp|.ods|.odt|.pdf|"
+                   + ".png|.ppt|.pptx|.ps|.rdf|.ris|.rtf|.sch|.txt|"
+                   + ".vcf|.wsdl|.xls|.xlsm|.xlsx|.xlt|.xml|.xsd|.xslt|"
+                   + ".zip")
+
+# filter pages with attachments on them
+df_output['attachment_exists'] = df_output['details'].str.contains(file_attachment, na=False)
+df_output = df_output.query('attachment_exists == True')
+
+# extract attachment
