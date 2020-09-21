04c48d584836047c6a12d2524e57fe414926ae62
avisionh <a_vision@hotmail.co.uk>
2020-09-21 17:47:32 +0100

    Extract pages with bold text in them
    This is so we can identify if they are being treated as fake headers because headers may be mistakenly being created under bold text.
    
    Also add to config file so report can run.

--- src/report_generators/fake_header_report_generator.py
+++ src/report_generators/fake_header_report_generator.py
@@ -1,4 +1,8 @@
 from src.report_generators.base_report_generator import BaseReportGenerator
+from src.helpers.preprocess_text import extract_subtext
+
+import pandas as pd
+from bs4 import BeautifulSoup
 
 
 class FakeHeaderReportGenerator(BaseReportGenerator):
@@ -14,4 +18,25 @@ class FakeHeaderReportGenerator(BaseReportGenerator):
         return "fake_header_page_report.csv"
 
     def process_page(self, content_item, html):
-        pass
+
+        # ignore empty fields
+        if pd.isna(content_item['details']):
+            return []
+
+        # extract text in bold
+        soup = BeautifulSoup(content_item['details'])
+        content_item['text_in_bold'] = [txt.string for txt in soup.findAll('b')]
+
+        # ignore content with no text in bold
+        if not content_item['text_in_bold']:
+            return []
+
+        # extract primary publishing organisation
+        content_item['primary_publishing_organisation'] = extract_subtext(text=content_item['organisations'],
+                                                                          key='primary_publishing_organisation',
+                                                                          index=1)
+
+        return [content_item['base_path'],
+                content_item['primary_publishing_organisation'],
+                content_item['publishing_app'],
+                content_item['document_type']]
