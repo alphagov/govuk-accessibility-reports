854236dd80fe2d557d80d2f9bab668355486375b
avisionh <a_vision@hotmail.co.uk>
2021-01-19 12:41:58 +0000

    Compute percentages and pivot results
    This is so accessibility team can work more easily with it.

--- notebooks/attachment_type_report_postprocess.py
+++ notebooks/attachment_type_report_postprocess.py
@@ -56,6 +56,40 @@ df["total_en_labelled_govuk_pages"] = n_govuk_pages
 # arrange by base_path
 df = df.sort_values(by=["base_path", "first_published_at", "attachment_type", "attachment_count"])
 
+# Report: Organisation ---
+df_org = df.value_counts(subset=["primary_publishing_organisation", "attachment_type"], sort=False)
+df_org = df_org.reset_index()
+df_totals = df.value_counts(subset=["primary_publishing_organisation"], sort=False)
+df_totals = df_totals.reset_index()
+df_org = df_org.merge(right=df_totals,
+                      how='left',
+                      on=["primary_publishing_organisation"],
+                      validate="many_to_one")
+df_org = df_org.rename(columns={"0_x": "counts_attachments",
+                                "0_y": "total_pages"})
+df_org["percentage_attachments"] = df_org["counts_attachments"] / df_org["total_pages"]
+df_org = pd.pivot_table(data=df_org,
+                        index=["primary_publishing_organisation"],
+                        columns="attachment_type",
+                        values="percentage_attachments").reset_index()
+df_org = df_org.sort_values(by="primary_publishing_organisation")
+
+# Report: Document Types ---
+df_doc = df.value_counts(subset=["document_type", "attachment_type"], sort=False).reset_index()
+df_totals = df.value_counts(subset=["document_type"], sort=False).reset_index()
+df_doc = df_doc.merge(right=df_totals,
+                      how='left',
+                      on=["document_type"],
+                      validate="many_to_one")
+df_doc = df_doc.rename(columns={"0_x": "counts_attachments",
+                                "0_y": "total_pages"})
+df_doc["percentage_attachments"] = df_doc["counts_attachments"] / df_doc["total_pages"]
+df_doc = pd.pivot_table(data=df_doc,
+                        index=["document_type"],
+                        columns="attachment_type",
+                        values="percentage_attachments").reset_index()
+df_doc = df_doc.sort_values(by="document_type")
+
 # export to csv
 df.to_csv(path_or_buf='data/attachment_type_report_process.csv',
           index=False)
