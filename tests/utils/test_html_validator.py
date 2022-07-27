15b19e966151f91e1b4bfa8f967124d55109db86
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-07-27 14:01:30 +0100

    Add tests for heading order with WrongStart flag

--- tests/utils/test_html_validator.py
+++ tests/utils/test_html_validator.py
@@ -2,8 +2,10 @@ import pytest
 
 from src.utils.html_validator import HtmlValidator
 from src.utils.table_accessibility_info import TableAccessibilityInfo
+from src.utils.heading_accessibility_info import HeadingAccessibilityInfo
 
 from tests.fixtures.tables import two_tables
+from tests.fixtures.html import good_html, wrong_start
 
 
 def test_validate_table_accessibility(two_tables):
@@ -11,3 +13,12 @@ def test_validate_table_accessibility(two_tables):
 
     assert HtmlValidator.validate_table_accessibility(two_tables).__dict__ == table_info.__dict__
 
+def test_validate_headings_accessibility_good_html(good_html):
+    html_info = HeadingAccessibilityInfo(headings="h1, h2", duplicate_h1s=False, bad_ordering=False, wrong_start=False)
+
+    assert HtmlValidator.validate_headings_accessibility(good_html).__dict__ == html_info.__dict__
+
+def test_validate_headings_accessibility_wrong_start(wrong_start):
+    html_info = HeadingAccessibilityInfo(headings="h2", wrong_start=True)
+
+    assert HtmlValidator.validate_headings_accessibility(wrong_start).__dict__ == html_info.__dict__
