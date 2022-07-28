9a66bcb1ba2a456bc3f9919a1fbf68d59152682a
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-07-28 14:51:46 +0100

    Fix header extractor (also handle new nav bar)

--- src/utils/html_extractor.py
+++ src/utils/html_extractor.py
@@ -5,14 +5,16 @@ from bs4 import BeautifulSoup
 class HtmlExtractor:
     header_regex = re.compile("^h[1-6]{1}$")
     excluded_headings = [
-        "Cookies on GOV.UK"
+        "Cookies on GOV.UK",
+        "Navigation menu",
+        "Find information and services",
+        "Search for a department and find out what the government is doing",
+        "Search",
+        "Popular on GOV.UK"
     ]
 
     @classmethod
     def extract_headings(cls, html):
         soup = BeautifulSoup(html, 'html5lib')
         matches = soup.find_all(cls.header_regex)
-
-        filtered_headings = filter(lambda match: match.text not in cls.excluded_headings, matches)
-
-        return list(map(lambda match: match.name, filtered_headings))
+        return [m.name for m in matches if m.text.strip() not in cls.excluded_headings]
