3c04c8521302434a226e813cfeafe7a4a80e4bea
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2022-07-28 14:07:59 +0100

    Add Primary Publishing Org to NonEnglishDocsReportGenerator

--- src/report_generators/non_english_docs_report_generator.py
+++ src/report_generators/non_english_docs_report_generator.py
@@ -8,7 +8,7 @@ import pandas as pd
 class NonEnglishDocsReportGenerator(BaseReportGenerator):
     @property
     def headers(self):
-        return ["base_path", "text", "text_languages", "detected_as_english"]
+        return ["base_path", "primary_publishing_organisation", "text", "text_languages", "detected_as_english"]
 
     @property
     def filename(self):
@@ -18,6 +18,8 @@ class NonEnglishDocsReportGenerator(BaseReportGenerator):
         if pd.isna(content_item['text']):
             return [content_item['base_path'], content_item['text'], [], content_item.get('detected_as_english',
                                                                                           default=False)]
+        # extract primary publishing organisation
+        primary_publishing_organisation = self.__primary_org(content_item['organisations'])
 
         content_item['text'] = str(content_item['text'])
 
@@ -27,7 +29,10 @@ class NonEnglishDocsReportGenerator(BaseReportGenerator):
         if 'en' in languages_dict and languages_dict['en'] > 0.5:
             content_item['detected_as_english'] = True
 
-        return [content_item['base_path'], content_item['text'], content_item['text_lang'],
+        return [content_item['base_path'],
+                primary_publishing_organisation,
+                content_item['text'],
+                content_item['text_lang'],
                 content_item.get('detected_as_english', default=False)]
 
     def detect_languages(self, text):
@@ -41,3 +46,32 @@ class NonEnglishDocsReportGenerator(BaseReportGenerator):
             return detect_langs(text)
         except LangDetectException:
             return []
+
+    @staticmethod
+    def __primary_org(content_item_orgs) -> str:
+        # Parses Content Item list of Organisations, extracts the Primary Publishing Organisation
+        #  and returns its title.
+        #  Falls back to first Organisation if there is no Primary Publishing Organisation.
+        #
+        # Structure of `orgs` would look like:
+        # {
+        #     'organisations': [
+        #         ('b548a09f-8b35-4104-89f4-f1a40bf3136d', 'Department for Work and Pensions', 'D10')
+        #     ],
+        #     'primary_publishing_organisation': [
+        #         ('b548a09f-8b35-4104-89f4-f1a40bf3136d', 'Department for Work and Pensions', 'D10')
+        #     ]
+        # }
+
+        primary = None
+        orgs = ast.literal_eval(content_item_orgs)
+
+        try:
+            if orgs is not None:
+                primary = (orgs.get('primary_publishing_organisation'))[0][1]
+            if primary is None:
+                primary = (orgs.get('organisations'))[0][1]
+        except TypeError:
+            primary = "NO ORG"
+
+        return primary
