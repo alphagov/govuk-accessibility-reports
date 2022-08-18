ec96bc0c42e7fd1cd85ee80dd780a49143e04a4b
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-08-18 14:57:55 +0100

    Add content item utils and link extractor

--- src/utils/html_extractor.py
+++ src/utils/html_extractor.py
@@ -1,6 +1,6 @@
+from src.utils.constants import ATTACHMENTS
 import re
 from bs4 import BeautifulSoup
-
 
 class HtmlExtractor:
     header_regex = re.compile("^h[1-6]{1}$")
@@ -18,3 +18,25 @@ class HtmlExtractor:
         soup = BeautifulSoup(html, 'html5lib')
         matches = soup.find_all(cls.header_regex)
         return [m.name for m in matches if m.text.strip() not in cls.excluded_headings]
+
+    @classmethod
+    def extract_links(cls, html):
+        soup = BeautifulSoup(html, 'html5lib')
+        links = soup.find_all(name="a", href=True)
+        return links
+
+    @classmethod
+    def extract_attachment_links(cls, html):
+        soup = BeautifulSoup(html, 'html5lib')
+        links = soup.find_all(name="a", href=True)
+        attachment_links = [l for l in links if HtmlExtractor.is_attachment_link(l)]
+        return links
+
+    @staticmethod
+    def link_extension(link):
+        parts = link.attrs['href'].split('.')
+        return parts[-1].lower()
+
+    @staticmethod
+    def is_attachment_link(link):
+        return ("." + HtmlExtractor.link_extension(link)) in ATTACHMENTS
